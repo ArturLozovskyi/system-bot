fff17cf3242a0689d1b040183a00c2808344fa51
Artur Lozovskyi <lozartval@gmail.com>
2020-03-28 15:14:43 +0200

    Created type of entity and allow deleting entity.

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const token = require('./token');
 const bot = new TelegramBot(token.url, { polling: true });
 
 const STATES = {
-  CREATE_ENTITY_FIRST_QUESTION: "CREATE_ENTITY_FIRST_QUESTION",
-  CREATE_ENTITY_SECOND_QUESTION: "CREATE_ENTITY_SECOND_QUESTION",
-  CREATED: "CREATED",
-  ANSWERING_QUESTIONS: "ANSWERING_QUESTIONS",
-  FINISH_ANSWERING_QUESTIONS: 'FINISH_ANSWERING_QUESTIONS',
-  WRITING_RULE: 'WRITING_RULE'
+  STATE_CREATE_ENTITY_NAME: 0,
+  STATE_CREATE_ENTITY_QUESTION: 1,
+  STATE_CREATE_ENTITY_TYPE: 2,
+  CREATED: 3,
+  ANSWERING_QUESTIONS: 4,
+  FINISH_ANSWERING_QUESTIONS: 5,
+  WRITING_RULE: 6,
+  STATE_DELETE_ENTITY: 7
+};
+
+const ENTITIES_TYPE = {
+  NUMERIC: 'Numeric',
+  BOOLEAN: 'Boolean'
 };
 
 const usersState = {};
@@ -20,37 +27,17 @@ let rule = '';
 
 entities.Age = {
   name: 'Age',
-  message: 'How old are you?'
+  message: 'How old are you?',
+  type: ENTITIES_TYPE.NUMERIC
 };
 
 entities.Car = {
   name: 'Car',
-  message: 'Do you have a car?'
+  message: 'Do you have a car?',
+  type: ENTITIES_TYPE.BOOLEAN
 };
 
-bot.onText(/\/help/, msg => {
-  const chatId = msg.chat.id
-  bot.sendMessage(chatId, 'Я умею показывать расписание, чтобы посмотреть его введи сообщение, которое содержит "расписание", а также отвечаю на некоторые сообщения) ')
-});
 
-///////////////////////////////////////////////////////////////////////
-
-
-bot.onText(/\/tasks/, msg => {
-  const chatId = msg.from.id;
-
-  bot.sendMessage(chatId, 'Что нужно сделать?', {
-    reply_markup:{
-      keyboard: [
-        ['Создать элемент'],
-        ['Посмотреть элементы'],
-        ['Пройти опрос'],
-        ['Записать правило'],
-        ['Закрыть']
-      ]
-    }
-  })
-});
   
 
 
@@ -61,14 +48,15 @@ bot.onText(/\/tasks/, msg => {
     const chatId = msg.from.id;
       
     switch(usersState[chatId]) {
-    case STATES.CREATE_ENTITY_FIRST_QUESTION:
+    case STATES.STATE_CREATE_ENTITY_NAME:
 
       const entityName = msg.text;
       entities[entityName] = {
         name: entityName,
-        message: ''
+        message: '',
+        type: null
       };
-      usersState[chatId] = STATES.CREATE_ENTITY_SECOND_QUESTION;
+      usersState[chatId] = STATES.STATE_CREATE_ENTITY_QUESTION;
       usersCurrentEntity[chatId] = entityName;
 
       bot.sendMessage(chatId, 'Напишите вопрос для этого элемента', {
@@ -78,12 +66,37 @@ bot.onText(/\/tasks/, msg => {
       });
       return;
 
-    case STATES.CREATE_ENTITY_SECOND_QUESTION:
+    case STATES.STATE_CREATE_ENTITY_QUESTION:
       
       entities[usersCurrentEntity[chatId]].message = msg.text;
-      usersState[chatId] = STATES.CREATED;
+      usersState[chatId] = STATES.STATE_CREATE_ENTITY_TYPE;
 
+      bot.sendMessage(chatId, 'Напишите тип элемента', {
+        reply_markup:{
+          keyboard: [
+            [ENTITIES_TYPE.NUMERIC],
+            [ENTITIES_TYPE.BOOLEAN],
+          ]
+        }
+      });
+      return;
+
+    case STATES.STATE_CREATE_ENTITY_TYPE:
+
+      entities[usersCurrentEntity[chatId]].type = msg.text;
+      usersState[chatId] = STATES.CREATED;
       bot.sendMessage(chatId, 'Спасибо за создание элемента', {
+        reply_markup:{
+          remove_keyboard: true
+        }
+      });
+      return;
+
+    case STATES.STATE_DELETE_ENTITY:
+      
+      delete entities[msg.text];
+      usersState[chatId] = STATES.CREATED;
+      bot.sendMessage(chatId, 'Элемент удален', {
         reply_markup:{
           remove_keyboard: true
         }
@@ -93,20 +106,17 @@ bot.onText(/\/tasks/, msg => {
     case STATES.ANSWERING_QUESTIONS:
 
       const entitiesKeys = Object.keys(entities);
-      console.log(entitiesKeys);
       const currentKey = entitiesKeys[usersCurrentNumberOfQuestion[chatId]];
-      console.log(currentKey);
       usersAnswers[chatId][currentKey] = msg.text;
 
 
       usersCurrentNumberOfQuestion[chatId]++;
 
       if(entitiesKeys.length === usersCurrentNumberOfQuestion[chatId]) {
-        const answers = JSON.stringify(usersAnswers[chatId]);
         normilizeAnswers(usersAnswers[chatId]);
         const result = checkAnswers(usersAnswers[chatId]);
-        console.log(usersAnswers[chatId]);
-        bot.sendMessage(chatId, 'Спасибо за Ваши ответы' + answers + result, {
+        const message = result === true ? 'Одобрено!' : 'Не одобрено!';
+        bot.sendMessage(chatId, 'Спасибо за Ваши ответы!\n' + message, {
           reply_markup:{
             remove_keyboard: true
           }
@@ -122,12 +132,24 @@ bot.onText(/\/tasks/, msg => {
     
       const nextKey = entitiesKeys[usersCurrentNumberOfQuestion[chatId]];
       const question = entities[nextKey].message;
+      const entityType = entities[nextKey].type;
       
-      bot.sendMessage(chatId, question, {
-        reply_markup:{
-          remove_keyboard: true
-        }
-      });
+      if(entityType === ENTITIES_TYPE.BOOLEAN) {
+        bot.sendMessage(chatId, question, {
+          reply_markup:{
+            keyboard: [
+              ['True'],
+              ['False']
+            ]
+          }
+        });
+      } else {
+        bot.sendMessage(chatId, question, {
+          reply_markup:{
+            remove_keyboard: true
+          }
+        });
+      }
       return;
 
     case STATES.WRITING_RULE:
@@ -162,14 +184,14 @@ bot.onText(/\/tasks/, msg => {
   if (msg.text === 'Создать элемент') {
     console.log('element');
     const chatId = msg.from.id;
-    usersState[chatId] = STATES.CREATE_ENTITY_FIRST_QUESTION;
+    usersState[chatId] = STATES.STATE_CREATE_ENTITY_NAME;
     bot.sendMessage(chatId, 'Напишите название элемента', {
       reply_markup:{
         remove_keyboard: true
       }
     });
     return;
-    }
+  }
 
     if(msg.text === 'Посмотреть элементы') {
       const message = createListOfEntities(entities).length > 0 ? createListOfEntities(entities) : 'Элементы еще не созданы';
@@ -200,19 +222,23 @@ bot.onText(/\/tasks/, msg => {
 
       const currentKey = entitiesKeys[usersCurrentNumberOfQuestion[chatId]];
       const question = entities[currentKey].message;
+      const entityType = entities[currentKey].type;
+      const messageOptions = {};
+
       usersAnswers[chatId] = {};
-      bot.sendMessage(chatId, question, {
-        reply_markup:{
+      if(entityType === ENTITIES_TYPE.BOOLEAN) {
+        messageOptions.reply_markup = {
+          keyboard: [
+            ['True'],
+            ['False']
+          ]
+        };          
+      } else {
+        messageOptions.reply_markup = {
           remove_keyboard: true
-        }
-      });
-      return;
-
-
-
-      ///////////////////////////////////////////////////
-
-
+        }; 
+      }
+      bot.sendMessage(chatId, question, messageOptions);
       return;
     }
 
@@ -233,6 +259,38 @@ bot.onText(/\/tasks/, msg => {
       });
     }
 
+    if(msg.text === 'Удалить элемент') {
+      usersState[chatId] = STATES.STATE_DELETE_ENTITY;
+      bot.sendMessage(chatId, 'Выберите название элемента, который нужно удалить', {
+        reply_markup:{
+          keyboard: [
+            Object.keys(entities)
+          ]
+        }
+      });
+      return;
+
+    }
+
+    if (msg.text === '/help') {
+      bot.sendMessage(chatId, 'Hello World!');
+    }
+
+    if (msg.text === '/tasks') {
+      bot.sendMessage(chatId, 'Что нужно сделать?', {
+        reply_markup:{
+          keyboard: [
+            ['Создать элемент'],
+            ['Посмотреть элементы'],
+            ['Пройти опрос'],
+            ['Записать правило'],
+            ['Удалить элемент'],
+            ['Закрыть']
+          ]
+        }
+      });
+    }
+
   });
 
 bot.on("polling_error", (err) => console.log(err));
@@ -240,9 +298,11 @@ bot.on("polling_error", (err) => console.log(err));
 function createListOfEntities(entities) {
   let message = '';
   let index = 0;
-  for(const key in entities) {
+  for(const entity of Object.values(entities)) {
     index++;
-    message+= `${index}. Название элемента: ${entities[key].name}\nВопрос для элемента: ${entities[key].message}\n\n`;
+    message += `${index}. Название элемента: ${entity.name}\n`;
+    message += `Вопрос для элемента: ${entity.message}\n`;
+    message += `Тип элемента: ${entity.type}\n`;
   }
   return message;
 }
@@ -250,10 +310,10 @@ function createListOfEntities(entities) {
 function normilizeAnswers (answers) {
   for(const key in answers) {
     if(isNaN(parseInt(answers[key], 10))) {
-      if(answers[key] === 'Да') {
+      if(answers[key] === 'True') {
         answers[key] = true;
       }
-      if(answers[key] === 'Нет') {
+      if(answers[key] === 'False') {
         answers[key] = false;
       }
     } else {
