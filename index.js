0e09a6b422790c8ba90cf76ffde13d5a3914b9fb
Artur <lozartval@gmail.com>
2020-03-28 13:32:29 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,267 +1,267 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = require('./token');
 const bot = new TelegramBot(token.url, { polling: true });
-const db = require('./db');
-var MongoClient = require('mongodb').MongoClient;
-var urlDB = 'mongodb://lozovartur77:45balo45__@ds237120.mlab.com:37120/telegram';
 
+const STATES = {
+  CREATE_ENTITY_FIRST_QUESTION: "CREATE_ENTITY_FIRST_QUESTION",
+  CREATE_ENTITY_SECOND_QUESTION: "CREATE_ENTITY_SECOND_QUESTION",
+  CREATED: "CREATED",
+  ANSWERING_QUESTIONS: "ANSWERING_QUESTIONS",
+  FINISH_ANSWERING_QUESTIONS: 'FINISH_ANSWERING_QUESTIONS',
+  WRITING_RULE: 'WRITING_RULE'
+};
 
+const usersState = {};
+const entities = {};
+const usersCurrentEntity = {};
+const usersCurrentNumberOfQuestion = {};
+const usersAnswers = {};
+let rule = '';
 
-const gameName = process.env.TELEGRAM_GAMENAME || 'onemoreclick';
-let url = process.env.URL || 'http://telegram-bot.zzz.com.ua'
+entities.Age = {
+  name: 'Age',
+  message: 'How old are you?'
+};
 
+entities.Car = {
+  name: 'Car',
+  message: 'Do you have a car?'
+};
 
-
-bot.onText(/\/help/, msg=> {
+bot.onText(/\/help/, msg => {
   const chatId = msg.chat.id
   bot.sendMessage(chatId, 'Я умею показывать расписание, чтобы посмотреть его введи сообщение, которое содержит "расписание", а также отвечаю на некоторые сообщения) ')
 });
 
 ///////////////////////////////////////////////////////////////////////
 
-var tasks = [];
-MongoClient.connect(urlDB, (err,database)=>{
-  if(err){
-    throw err;
-    console.log(err);
-  }
-  else{
-    console.log('Connected!');
-    database.collection('tasks').find().toArray(function(err, results){
-      tasks = results;
-      database.close();
-    });
-  }   
-});
 
-bot.onText(/\/tasks/, msg=> {
+bot.onText(/\/tasks/, msg => {
   const chatId = msg.from.id;
 
   bot.sendMessage(chatId, 'Что нужно сделать?', {
     reply_markup:{
       keyboard: [
-        ['Просмотреть'],
-        ['Напомнить','Удалить'],
+        ['Создать элемент'],
+        ['Посмотреть элементы'],
+        ['Пройти опрос'],
+        ['Записать правило'],
         ['Закрыть']
       ]
     }
   })
 });
   
-  bot.onText(/^Напомнить$/, msg=> {
-    const chatId = msg.from.id;
-    bot.sendMessage(chatId, 'Напиши что нужно добавить в таком виде : "Напомни ... "', {
-      reply_markup:{
-        remove_keyboard: true
-      }
-    })
-  });
-
-
-  bot.onText(/^Напомни (.+)$/, function (msg, match) {
-    const chatId = msg.from.id;
-    var text = match[1];
-    MongoClient.connect(urlDB, (err,database)=>{
-      if(err){
-        throw err;
-        console.log(err);
-      }
-      else{
-        console.log('Connected!');
-        var collection = database.collection('tasks');
-        var task = {
-          id: chatId,
-          text: text
-        };
-        collection.insertOne(task,(err,result)=>{
-          if(err){
-            throw err;
-            console.log(err);
-          }
-          console.log(result.ops);
-
-          database.collection('tasks').find().toArray(function(err, results){
-            tasks = results;
-            console.log(tasks)
-            database.close();
-          });
-        });
-      }
-    });
-    bot.sendMessage(chatId, 'Хорошо)');
-  });
-
-  
-  bot.onText(/^Просмотреть$/, msg=> {  
-    var i = 0;
-    const chatId = msg.from.id;
-    tasks.forEach(task=>{
-      if(chatId === task.id){
-        bot.sendMessage(task.id, 'Напоминаю, что вы должны: '+ task.text), {
-          reply_markup:{
-            remove_keyboard: true
-          }
-        };
-        i++;
-      }
-    })
-    if(i==0){
-    bot.sendMessage(chatId, 'Ничего не запланировано', {
-      reply_markup:{
-        remove_keyboard: true
-      }
-    });
-    }
-  });
-
-
-  bot.onText(/^Удалить$/, msg=> {
-    var i = 0;
-    const chatId = msg.from.id;
-    var k = 0;
-    bot.sendMessage(chatId, 'Напиши что нужно удалить в таком виде : "Удали ... (номер)"',{
-      reply_markup:{
-        remove_keyboard: true
-      }
-    });
-
-    tasks.forEach(task=>{
-      if(chatId === task.id){
-        k++;
-        bot.sendMessage(task.id, k+'. '+ task.text), {
-          reply_markup:{
-            remove_keyboard: true
-          }
-        };
-        i++;
-      }
-    })
-    if(i==0){
-    bot.sendMessage(chatId, 'Ничего не запланировано', {
-      reply_markup:{
-        remove_keyboard: true
-      }
-    });
-    }
-});
-
-
-  bot.onText(/^Удали (.+)$/, function (msg, match) {
-    const chatId = msg.from.id;
-    var number = match[1]-1;
-    var newTask = [];
-    tasks.forEach(task=>{
-      if(task.id = chatId){
-        newTask.push(task);
-      }
-    });
-    if((number=>0)&&(number<newTask.length)&&(newTask.length!=0)){
-      MongoClient.connect(urlDB, (err,database)=>{
-        if(err){
-          throw err;
-          console.log(err);
-        }  
-        database.collection('tasks').deleteOne({id: newTask[number].id,text:newTask[number].text}, function(err, result){       
-          if(err){
-            throw err;
-          }
-      });
-      database.collection('tasks').find().toArray(function(err, results){
-        if(err){
-          throw err;
-        }
-        tasks = results;
-      });
-    });
-      bot.sendMessage(chatId, 'Удалила)');  
-  }
-  else{
-    bot.sendMessage(chatId, 'Напиши еще раз)');  
-  }
-});
-
 
 
 
 ///////////////////////////////////////////////////////////////////////
 
-bot.on('message', msg=>{
-  
-  const chatId = msg.chat.id;
-  if(msg.chat.type === 'group' && msg.from.id === 565126483) {
-    bot.sendMessage(chatId, 'Артур только что написал: ' + msg.text);
+  bot.on('message', msg => {
+    const chatId = msg.from.id;
+      
+    switch(usersState[chatId]) {
+    case STATES.CREATE_ENTITY_FIRST_QUESTION:
+
+      const entityName = msg.text;
+      entities[entityName] = {
+        name: entityName,
+        message: ''
+      };
+      usersState[chatId] = STATES.CREATE_ENTITY_SECOND_QUESTION;
+      usersCurrentEntity[chatId] = entityName;
+
+      bot.sendMessage(chatId, 'Напишите вопрос для этого элемента', {
+        reply_markup:{
+          remove_keyboard: true
+        }
+      });
+      return;
+
+    case STATES.CREATE_ENTITY_SECOND_QUESTION:
+      
+      entities[usersCurrentEntity[chatId]].message = msg.text;
+      usersState[chatId] = STATES.CREATED;
+
+      bot.sendMessage(chatId, 'Спасибо за создание элемента', {
+        reply_markup:{
+          remove_keyboard: true
+        }
+      });
+      return;
+
+    case STATES.ANSWERING_QUESTIONS:
+
+      const entitiesKeys = Object.keys(entities);
+      console.log(entitiesKeys);
+      const currentKey = entitiesKeys[usersCurrentNumberOfQuestion[chatId]];
+      console.log(currentKey);
+      usersAnswers[chatId][currentKey] = msg.text;
+
+
+      usersCurrentNumberOfQuestion[chatId]++;
+
+      if(entitiesKeys.length === usersCurrentNumberOfQuestion[chatId]) {
+        const answers = JSON.stringify(usersAnswers[chatId]);
+        normilizeAnswers(usersAnswers[chatId]);
+        const result = checkAnswers(usersAnswers[chatId]);
+        console.log(usersAnswers[chatId]);
+        bot.sendMessage(chatId, 'Спасибо за Ваши ответы' + answers + result, {
+          reply_markup:{
+            remove_keyboard: true
+          }
+        });
+        
+        
+        usersState[chatId] = STATES.FINISH_ANSWERING_QUESTIONS;
+        usersCurrentNumberOfQuestion[chatId] = 0;
+        usersAnswers[chatId] = {};
+        return;
+      }
+
+    
+      const nextKey = entitiesKeys[usersCurrentNumberOfQuestion[chatId]];
+      const question = entities[nextKey].message;
+      
+      bot.sendMessage(chatId, question, {
+        reply_markup:{
+          remove_keyboard: true
+        }
+      });
+      return;
+
+    case STATES.WRITING_RULE:
+      rule = msg.text;
+      try {
+        eval(`with(entities) { ${rule.replace(/&&/g, '&').replace(/\|\|/g, '&')} }`);
+        usersState[chatId] = STATES.CREATED;
+        bot.sendMessage(chatId, 'Правило успешно записано', {
+          reply_markup:{
+            remove_keyboard: true
+          }
+        });
+      } catch (error) {
+        rule = '';
+        if (error instanceof SyntaxError) {
+          bot.sendMessage(chatId, 'Правило задано с ошибкой, попробуйте еще раз: неправильный синтаксис', {
+            reply_markup:{
+              remove_keyboard: true
+            }
+          });
+        } else if (error instanceof ReferenceError) {
+          const key = error.message.split(' ')[0];
+          bot.sendMessage(chatId, `Правило задано с ошибкой, попробуйте еще раз: элемента с ключом ${key} не найдено`, {
+            reply_markup:{
+              remove_keyboard: true
+            }
+          });
+        }
+      }
   }
 
-  var message = msg.text.toLowerCase();
-
-  if(message == 'закрыть'){
-    bot.sendMessage(chatId, 'Закрываю', {
+  if (msg.text === 'Создать элемент') {
+    console.log('element');
+    const chatId = msg.from.id;
+    usersState[chatId] = STATES.CREATE_ENTITY_FIRST_QUESTION;
+    bot.sendMessage(chatId, 'Напишите название элемента', {
       reply_markup:{
         remove_keyboard: true
       }
-    })
-  }
+    });
+    return;
+    }
 
-  else if(message.indexOf('расписание') != -1 || message == "/schedule"){
-    bot.sendMessage(chatId, 'Выбери день ', {
-      reply_markup:{
-        keyboard: [
-          ['Понедельник','Вторник'],
-          ['Среда','Четверг'],
-          ['Пятница', 'Закрыть']
-        ]
+    if(msg.text === 'Посмотреть элементы') {
+      const message = createListOfEntities(entities).length > 0 ? createListOfEntities(entities) : 'Элементы еще не созданы';
+      bot.sendMessage(chatId, message, {
+        reply_markup:{
+          remove_keyboard: true
+        },
+        parse_mode: 'HTML'
+      });
+      return;
+    }
+
+    if(msg.text === 'Пройти опрос') {
+      if(rule.length === 0) {
+        bot.sendMessage(chatId, 'Сначала создайте правило', {
+          reply_markup:{
+            remove_keyboard: true
+          }
+        });
+        return;
       }
-    })
+      usersState[chatId] = STATES.ANSWERING_QUESTIONS;
+      usersCurrentNumberOfQuestion[chatId] = 0;
+
+      ///////////////////////////////////////////////////
+
+      const entitiesKeys = Object.keys(entities);
+
+      const currentKey = entitiesKeys[usersCurrentNumberOfQuestion[chatId]];
+      const question = entities[currentKey].message;
+      usersAnswers[chatId] = {};
+      bot.sendMessage(chatId, question, {
+        reply_markup:{
+          remove_keyboard: true
+        }
+      });
+      return;
+
+
+
+      ///////////////////////////////////////////////////
+
+
+      return;
+    }
+
+    if(msg.text === 'Записать правило') {
+      if(Object.keys(entities).length === 0) {
+        bot.sendMessage(chatId, 'Сначала создайте элементы', {
+          reply_markup:{
+            remove_keyboard: true
+          }
+        });
+        return;
+      }
+      usersState[chatId] = STATES.WRITING_RULE;
+      bot.sendMessage(chatId, 'Запишите правило', {
+        reply_markup:{
+          remove_keyboard: true
+        }
+      });
+    }
+
+  });
+
+bot.on("polling_error", (err) => console.log(err));
+
+function createListOfEntities(entities) {
+  let message = '';
+  let index = 0;
+  for(const key in entities) {
+    index++;
+    message+= `${index}. Название элемента: ${entities[key].name}\nВопрос для элемента: ${entities[key].message}\n\n`;
   }
+  return message;
+}
 
-  if(message.indexOf('спасибо') != -1){
-    bot.sendMessage(chatId, 'Не за что)')
+function normilizeAnswers (answers) {
+  for(const key in answers) {
+    if(isNaN(parseInt(answers[key], 10))) {
+      if(answers[key] === 'Да') {
+        answers[key] = true;
+      }
+      if(answers[key] === 'Нет') {
+        answers[key] = false;
+      }
+    } else {
+      answers[key] = parseInt(answers[key]);
+    }
   }
-  if(message.indexOf('привет') != -1){
-    bot.sendMessage(chatId, 'Привет, ' +  msg.from. first_name)
-  }
+}
 
-
-  switch(msg.text)
-  {
-    case 'Понедельник':
-    db.days[0].lessons.forEach(element => {
-    bot.sendMessage(msg.chat.id, element.id + "." +element.name)
-    })
-    break
-    case 'Вторник':
-    db.days[1].lessons.forEach(element => {
-    bot.sendMessage(msg.chat.id, element.id + "." +element.name)
-    })
-    break
-    case 'Среда':
-    bot.sendMessage(msg.chat.id, 'Отработки')
-    break
-    case 'Четверг':
-    db.days[3].lessons.forEach(element => {
-    bot.sendMessage(msg.chat.id, element.id + "." +element.name)
-    })
-    break
-    case 'Пятница':
-    db.days[4].lessons.forEach(element => {
-    bot.sendMessage(msg.chat.id, element.id + "." +element.name)
-    })
-    break
-  }
-});
-
-
-
-
-// Matches /start
-bot.onText(/\/play/, function onPhotoText(msg) {
-  bot.sendGame(msg.chat.id, gameName);
-});
-
-// Handle callback queries
-bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-  bot.answerCallbackQuery(callbackQuery.id, { url });
-});
-
-
-
+function checkAnswers(answers) {
+  return eval(`with(answers) { ${rule} }`);
+}
